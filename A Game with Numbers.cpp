e717be350af3fefbd4c9c5fccf293e3db5157160
Adittya Raj <raj21adittya@gmail.com>
2019-06-06 23:20:55 +0400

    Create A Game with Numbers.cpp

---
+++ A Game with Numbers.cpp
@@ -0,0 +1,16 @@
+//https://www.spoj.com/problems/NGM/
+//@bl!nd_m0n3y
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	long long n;
+	cin>>n;
+	if(n%10==0)
+	cout<<2;
+	else
+    cout<<1<<endl<<n%10<<endl;
+	
+	
+}
