19aa546911bcb5716f98f6fea7125c0685fbde92
Adittya Raj <raj21adittya@gmail.com>
2019-06-08 22:59:15 +0400

    Create DOTA HEROES.cpp

---
+++ DOTA HEROES.cpp
@@ -0,0 +1,26 @@
+//https://www.spoj.com/problems/DOTAA/
+//@bl!nd_m0n3y
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T,n,i,m,d,h[500],cnt=0;
+    cin>>T;
+    while(T--)
+    {
+        cin>>n>>m>>d;
+        cnt=0;
+        for(i=0;i<n;i++)
+            {
+                cin>>h[i];
+                while((h[i]-=d)>0) {
+                cnt++;
+            }
+            }
+
+          if(cnt>=m) cout<<"YES"<<endl;
+          else cout<<"NO"<<endl;
+    }
+    return 0;
+}
