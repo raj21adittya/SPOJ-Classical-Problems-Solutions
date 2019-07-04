29748148bc8b48dc96c7de1d29b36046ed507af3
Adittya Raj <raj21adittya@gmail.com>
2019-07-04 12:31:13 +0400

    Create Hubulullu.cpp

---
+++ Hubulullu.cpp
@@ -0,0 +1,19 @@
+//https://www.spoj.com/problems/HUBULLU/
+//@bl1nd_m0n3y
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n,s;
+		cin>>n>>s;
+		if(s==0)
+		cout<<"Airborne wins."<<endl;
+		else
+		cout<<"Pagfloyd wins."<<endl;
+	}
+}
